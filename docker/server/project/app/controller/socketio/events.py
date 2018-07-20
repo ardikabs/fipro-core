538d7808cdc89f56a766d49c3183e9ac6697749d
ardikabs <ardikabs@gmail.com>
2018-07-20 17:14:21 +0700

    websocket is ready

--- docker/server/project/app/controller/socketio/events.py
+++ docker/server/project/app/controller/socketio/events.py
@@ -8,14 +8,29 @@ from flask_login import(
 )
 from . import socketio
 from .worker import Worker
+from app.decorators import authenticated_only
 
 worker = None
 
+############################
+'''
+Events SocketIO
+'''
+############################
+
 @socketio.on("get_recent_attacks", namespace="/socket.io")
+@authenticated_only
 def request_recent_attacks(data):
     global worker
     worker = Worker(socketio)
     socketio.start_background_task(target=worker.run, args=(current_app._get_current_object(),), **{'identifier':current_user.identifier})
+
+
+
+
+
+
+
 
 ############################
 '''
@@ -24,10 +39,12 @@ Connection SocketIO
 ############################
 
 @socketio.on("connect", namespace="/socket.io")
+@authenticated_only
 def on_connect():
     print ("Connected to Socket.io with user: {}".format(current_user.fullname()))
 
 @socketio.on("disconnect", namespace="/socket.io")
+@authenticated_only
 def on_disconnect():
     if worker is not None:
         worker.stop()
