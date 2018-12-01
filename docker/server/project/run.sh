40adde11d2c19d24b157c7a91af45e70712ab6cf
Ardika Bagus Saputro <ardika.saputro@gdn-commerce.com>
2018-12-01 16:13:19 +0700

    change config for redis

--- docker/server/project/run.sh
+++ docker/server/project/run.sh
@@ -2,5 +2,5 @@
 
 celery worker -A celery_worker.celery --loglevel=info &
 celery beat -A celery_worker.celery --loglevel=info &
-gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8000 --reload wsgi
+gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8080 --reload wsgi
 
